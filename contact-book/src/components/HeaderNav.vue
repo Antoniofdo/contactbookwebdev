898f368e0e8581704c70f582a1cd3d0522f5df90
Antoniofdo <fern0225@algonquinlive.com>
2026-04-21 12:27:36 -0400

    push

---
+++ contact-book/src/components/HeaderNav.vue
@@ -0,0 +1,13 @@
+<template>
+  <header class="topbar">
+    <h1>Contact Book</h1>
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/add">Add Contact</RouterLink>
+    </nav>
+  </header>
+</template>
+
+<script setup>
+import { RouterLink } from 'vue-router';
+</script>
